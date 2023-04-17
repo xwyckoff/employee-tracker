8a1a56aca3035d31f688a36a565d8b8ee0bd476c
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-04-17 01:07:09 -0400

    added functions I think I will need, subject to change

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
+
+//create the connection to the db
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'testPass1?',
+    database: 'buisness_db'
+});
+
+async function getAllDepartments() {
+
+}
+
+async function getAllRoles() {
+
+}
+
+async function getAllEmployees() {
+
+}
+
+async function addDepartment() {
+
+}
+
+async function addRole() {
+
+}
+
+async function addEmployee() {
+
+}
+
+async function updateEmployee() {
+    
+}
