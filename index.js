588da7699e11bb6a12cc33fc8e2923e463785a83
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-04-17 01:32:12 -0400

    added functionality to functions to view each table

--- index.js
+++ index.js
@@ -7,33 +7,77 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'testPass1?',
-    database: 'buisness_db'
+    database: 'business_db'
 });
 
-async function getAllDepartments() {
+//returns all of the departments from the department table
+function getAllDepartments() {
+    db.query(`SELECT * FROM department`, (err, results) => {
+        if (err) console.log(err)
+        else {
+            console.table(results);
+        }
+    })
+}
+
+//returns all of the roles from the role table
+function getAllRoles() {
+    db.query(`SELECT * FROM role`, (err, results) => {
+        if (err) console.log(err)
+        else {
+            console.table(results);
+        }
+    })
+}
+
+//returns all of the employees from the employee table
+function getAllEmployees() {
+    db.query(`SELECT * FROM employee`, (err, results) => {
+        if (err) console.log(err)
+        else {
+            console.table(results);
+        }
+    })
+}
+
+function addDepartment() {
 
 }
 
-async function getAllRoles() {
+function addRole() {
 
 }
 
-async function getAllEmployees() {
+function addEmployee() {
 
 }
 
-async function addDepartment() {
+function updateEmployee() {
 
 }
 
-async function addRole() {
-
+function mainMenu() {
+    inquirer.prompt([
+        {
+            name: 'option',
+            message: 'What would you like to do?',
+            type: 'list',
+            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update Employee Role']
+        }
+    ])
+    .then(answer => {
+        switch(answer.option) {
+            case "View All Departments":
+                getAllDepartments();
+                break;
+            case "View All Roles":
+                getAllRoles();
+                break;
+            case "View All Employees":
+                getAllEmployees();
+                break;
+        }
+    })
 }
 
-async function addEmployee() {
-
-}
-
-async function updateEmployee() {
-    
-}
+mainMenu();
